133129c3240459a519c9812dbeb0a09a0cad14d6
= <=>
2023-10-30 10:26:09 +0530

    Second

---
+++ src/main/java/com/lti/ltidemo/LtidemoApplication.java
@@ -0,0 +1,13 @@
+package com.lti.ltidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtidemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lti/ltidemo/LtidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lti.ltidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LtidemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
